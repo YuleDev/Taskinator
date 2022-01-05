48e80e9d1ca7ba67e0e2dd8c75d41356cd19d2d5
thatblondetitan <koleervine@gmail.com>
2022-01-05 12:24:55 -0700

    JS changes
    changed click listener to event for form

--- Assets/JS/index.js
+++ Assets/JS/index.js
@@ -1,18 +1,17 @@
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-/* create task handler code added below */
+var createTaskHandler = function(event) {
 
-var createTaskHandler = function() {
+  event.preventDefault();
+
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     listItemEl.textContent = "This is a new task.";
     tasksToDoEl.appendChild(listItemEl);
   }
 
-/* end task handler code */
-
-buttonEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler);
 
 var buttonEl = document.querySelector("#save-task");
 console.log(buttonEl)
